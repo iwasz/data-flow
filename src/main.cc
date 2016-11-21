2e4c772e74b97b9e83d6eb28ba3f22c17e330222
Iwasz <lukasz.iwaszkiewicz@gmail.com>
2016-11-21 23:54:29 +0100

    Basic dataflow works

--- src/main.cc
+++ src/main.cc
@@ -84,28 +84,82 @@ public:
         virtual bool inputsOk () const { return InputStrategy::check (inputs); }
         virtual bool outputsOk () const { return RequireAllFree<OUTPUTS_NO>::check (outputs); }
 
-protected:
+        // protected:
         Arc *inputs[INPUTS_NO];
         Arc *outputs[OUTPUTS_NO];
 };
 
 class Add : public AbstractNode<2, 1, RequireAllFull<2>> {
 public:
-        void process ();
+        void process () { outputs[0]->put (inputs[0]->get () + inputs[1]->get ()); }
 };
 
-void Add::process () { outputs[0]->put (inputs[0]->get () + inputs[1]->get ()); }
-}
+class Source : public INode {
+public:
+        virtual ~Source () {}
+        bool inputsOk () const { return true; }
+        bool outputsOk () const { return !output->isFull (); }
 
-using namespace flow;
+        // protected:
+        Arc *output;
+};
+
+class Const : public Source {
+public:
+        Const (int i) : value (i) {}
+        virtual ~Const () {}
+        void process () { output->put (value); }
+
+private:
+        int value;
+};
+
+class Sink : public INode {
+public:
+        virtual ~Sink () {}
+        bool inputsOk () const { return input->isFull (); }
+        bool outputsOk () const { return true; }
+
+        // protected:
+        Arc *input;
+};
+
+class Console : public Sink {
+public:
+        virtual ~Console () {}
+        void process () { std::cerr << input->get () << std::endl; }
+};
+}
 
 int main (int argc, char **argv)
 {
 
-        NodeVector nodes;
+        flow::NodeVector nodes;
+
+        flow::Const c1 (2);
+        flow::Const c2 (3);
+        flow::Add a;
+        flow::Console l;
+
+        nodes.push_back (&l);
+        nodes.push_back (&c1);
+        nodes.push_back (&a);
+        nodes.push_back (&c2);
+
+        flow::Arc a1;
+        c1.output = &a1;
+        a.inputs[0] = &a1;
+
+        flow::Arc a2;
+        c2.output = &a2;
+        a.inputs[1] = &a2;
+
+        flow::Arc a3;
+        a.outputs[0] = &a3;
+        l.input = &a3;
 
         while (true) {
-                for (INode *node : nodes) {
+                for (flow::INode *node : nodes) {
                         if (node->inputsOk () && node->outputsOk ()) {
                                 node->process ();
                         }
